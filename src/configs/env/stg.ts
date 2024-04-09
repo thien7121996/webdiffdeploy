5a9ce5dc579af3bbf239bbcd279c96853bd438ad
thien-le-corize <thien.le@corize.co.jp>
2024-04-09 10:35:36 +0700

    fix deploy v4

--- src/configs/env/stg.ts
+++ src/configs/env/stg.ts
@@ -1,10 +1,10 @@
 import { Config } from '@/types/config';
 const config: Config = {
   client: {
-    origin: 'http://localhost:3000',
+    origin: 'https://webdiff-lovat.vercel.app',
   },
   api: {
-    origin: 'http://localhost:3000/api',
+    origin: 'https://webdiff-lovat.vercel.app/api',
   },
   queueServer: { origin: 'https://socket-queue.onrender.com' },
   cookie: {
